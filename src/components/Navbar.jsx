fa77ce6260f43a991d0c3d02a7ebe21e1eb871be
brainyjustin12 <brainyjustin@outlook.com>
2026-04-15 14:08:52 +0200

    add logo in correct way

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,7 +2,7 @@ import { Link, useNavigate } from 'react-router-dom';
 import { useAuth } from '../context/AuthContext';
 import { LogOut, Menu, X } from 'lucide-react';
 import { useState } from 'react';
-import avatar from '../assets/avatar.jpg';
+import avatar from '../assets/avatar.png';
 
 export default function Navbar() {
   const { user, logout } = useAuth();
